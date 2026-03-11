fa4a7fd71ab412ae8893ef75b0ff165540558d80
ThanhDan <dinhthanhdan2004@gmail.com>
2026-03-11 08:23:45 +0700

    sum(a,b)

---
+++ main.py
@@ -0,0 +1,9 @@
+# Nhập hai số từ bàn phím
+a = float(input("Nhập số thứ nhất: "))
+b = float(input("Nhập số thứ hai: "))
+
+# Tính tổng
+tong = a + b
+
+# In kết quả
+print("Tổng của hai số là:", tong)
